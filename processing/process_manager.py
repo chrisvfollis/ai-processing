79be8c9c130ee98439d7aaa3a86806d102e3b1b6
Chris Follis <chrisvfollis@gmail.com>
2024-12-19 18:39:59 -0800

    temporarily disable multiprocessing

--- processing/process_manager.py
+++ processing/process_manager.py
@@ -139,12 +139,19 @@ def main(stride=15):
         time_prefix = utilities.parse_clip_filename(queue_block[0][0], data='time')
         timestamp = utilities.frame_timestamp(time_prefix)
         
-        with multiprocessing.Pool(processes=4) as pool:
-            tasks = [
-                (row, credentials, weights_paths, device, stride, time_prefix)
-                for row in queue_block
-            ]
-            pool.starmap(process_row, tasks)
+        # with multiprocessing.Pool(processes=4) as pool:
+        #     tasks = [
+        #         (row, credentials, weights_paths, device, stride, time_prefix)
+        #         for row in queue_block
+        #     ]
+        #     pool.starmap(process_row, tasks)
+
+        tasks = [
+            (row, credentials, weights_paths, device, stride, time_prefix)
+            for row in queue_block
+        ]
+        for task in tasks:
+            process_row(*task)
 
         io_utils.post_events_to_webapp(time_prefix)
         response = requests.post(
