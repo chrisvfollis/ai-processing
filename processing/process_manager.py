fac9b709910064a73e8a74f2f35b8896a9aa0fc1
Chris Follis <chrisvfollis@gmail.com>
2024-12-19 19:16:47 -0800

    adjust memory limit again

--- processing/process_manager.py
+++ processing/process_manager.py
@@ -70,7 +70,7 @@ def process_row(row, credentials, weights_paths, device, stride, time_prefix):
                 tf.config.experimental.set_memory_growth(gpu, True)
             tf.config.experimental.set_virtual_device_configuration(
                 gpus[0],
-                [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)]
+                [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=2048)]
             )
             print("TensorFlow GPU memory configured.")
         except RuntimeError as e:
