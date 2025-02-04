072ae41c1458829350bee0acfd86c17db85e8f04
Chris Follis <chrisvfollis@gmail.com>
2025-02-04 12:27:46 -0800

    Improve EC2 SCP download function

--- admin/ec2_util.py
+++ admin/ec2_util.py
@@ -1,28 +1,42 @@
 import subprocess
 import os
 
-def scp_download(remote_path, local_destination, pem_path, remote_host, remote_user='ubuntu'):
 
-    mappings = {
-        ''
-    }
+def scp_download(remote_dir, local_dir, remote_host, remote_user='ubuntu',
+                 remote_path='/home/ubuntu/timemanager/', pem='timemanager.pem'):
+    local_path = os.path.dirname(os.getcwd())
 
-
+    codebase_dirs = ['input_files', 'output_files', 'output_files/event_imgs',
+                     'intermediate_output', 'admin/testing/output',
+                     'admin/testing/output/detections']
+    
+    if remote_dir in codebase_dirs:
+        remote_path = os.path.join(remote_path, remote_dir)
+    else:
+        remote_path = remote_dir
+    
+    if local_dir in codebase_dirs:
+        local_path = os.path.join(local_path, local_dir)
+    else:
+        local_path = local_dir
+    
+    if remote_dir == local_dir:
+        remote_path = os.path.join(remote_path, '*')
+    
     recursive_flag = '-r' if remote_path.endswith('/') else ''
 
     scp_command = [
         'scp',
-        '-i', pem_path,
+        '-i', pem,
         recursive_flag,
         f"{remote_user}@{remote_host}:{remote_path}",
-        local_destination
+        local_path
     ]
 
     scp_command = [arg for arg in scp_command if arg]
 
     try:
         subprocess.run(scp_command, check=True)
-        print(f"Successfully copied {remote_path} to {local_destination}")
+        print(f"Successfully copied {remote_path} to {local_path}")
     except subprocess.CalledProcessError as e:
         print(f"Error during SCP: {e}")
-
