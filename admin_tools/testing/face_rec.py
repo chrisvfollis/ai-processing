7f2e19034a8d770b86ee6eed09b5cc4046a93cb8
Chris Follis <chrisvfollis@gmail.com>
2025-02-05 22:07:02 -0800

    Fix small typo

--- admin_tools/testing/face_rec.py
+++ admin_tools/testing/face_rec.py
@@ -14,7 +14,7 @@ from models.face_iq import FaceIq
 def run_function(fx, video):
     video_path = os.path.join('input', video)
 
-    face_iq = FaceIq('Facenet512', 'retinaface', face_ir='../../files/input/faces',
+    face_iq = FaceIq('Facenet512', 'retinaface', face_dir='../../files/input/faces',
                      db_path='../../files/data.db')
 
     if fx == 'whole_image':
