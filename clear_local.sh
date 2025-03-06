f816aa0153e49c35fcb010e572378f7ccb186c9c
Chris Follis <chrisvfollis@gmail.com>
2025-03-06 15:04:59 -0800

    Easy cleanup shell script

--- clear_local.sh
+++ clear_local.sh
@@ -1,27 +1,7 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-  echo "Missing argument: <config_filename>"
-  exit 1
-fi
+rm files/output/*.pkl
+rm files/output/*.hdf5
 
-CONFIG_FILE="$1"
-
-RESULT=$(python3 -c "
-from admin_utils import ec2_public_dns
-pem_path, remote_user, public_dns = ec2_public_dns('$CONFIG_FILE')
-print(f'{pem_path} {remote_user} {public_dns}')
-")
-
-PEM_PATH=$(echo $RESULT | awk '{print $1}')
-REMOTE_USER=$(echo $RESULT | awk '{print $2}')
-PUBLIC_DNS=$(echo $RESULT | awk '{print $3}')
-
-if [ -z "$PUBLIC_DNS" ]; then
-  echo "Failed to retrieve public DNS. Is the instance running?"
-  exit 1
-fi
-
-echo "Connecting to $PUBLIC_DNS..."
-
-ssh -i "$PEM_PATH" "$REMOTE_USER@$PUBLIC_DNS"
+rm files/output/runtime_data/*.xlsx
+rm files/output/videos/*.mp4
