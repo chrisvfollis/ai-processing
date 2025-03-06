3ba9346099e9ac2b417275c5def63f71e8733f4f
Chris Follis <chrisvfollis@gmail.com>
2025-03-06 15:46:00 -0800

    Additional shell script tooling

--- cleanup.sh
+++ cleanup.sh
@@ -6,5 +6,5 @@ rm -f files/output/*.hdf5
 rm -f files/output/runtime_data/*.xlsx
 rm -f files/output/videos/*.mp4
 
-journalctl --rotate
-journalctl --vacuum-time=1s
+sudo journalctl --rotate
+sudo journalctl --vacuum-time=1s
---
+++ logs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [[ "$1" == "--forward" ]]; then
+    sudo journalctl -u ai-process.service
+else
+    sudo journalctl -u ai-process.service --reverse
+files
