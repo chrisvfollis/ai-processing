68c40733fad45cf5a3ad435cd4cdbc37e22eb991
Chris Follis <chrisvfollis@gmail.com>
2025-05-30 03:40:29 -0700

    Expose RetinaFace class at the package level

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 from .yolov4 import YOLOv4
 from .yolox import YoloX
 from .osnet import OSNet
+from .retinaface import RetinaFace
 from .centerface import CenterFace
 from .clearface import ClearFace
 from .facenet import FaceNet512
@@ -11,6 +12,7 @@ __all__ = [
     'YOLOv4',
     'YoloX',
     'OSNet',
+    'RetinaFace',
     'CenterFace',
     'ClearFace',
     'FaceNet512',
